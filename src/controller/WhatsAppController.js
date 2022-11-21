a2ab9db953aadaee5296c9701b3285956f6e0a46
Matheus <mfabryny@minsait.com>
2022-11-21 12:49:05 -0300

    Creating attributes for each div in the html that has id

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,14 @@
 class WhatsAppController {
     constructor() {
         console.log("criou")
+
+        this.loadElements()
+    }
+
+    loadElements() {
+        this.el = {}
+        document.querySelectorAll('[id]').forEach(element => {  /* Todos que possuem ID */
+            this.el[Format.getCamelCase(element.id)] = element; /* Importante padronização na nomeclatura dos ID */
+        })
     }
 }
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,3 +1,8 @@
 class Format {
-    
+    static getCamelCase(txt){
+        let div = document.createElement('div');
+        div.innerHTML = `<div data-${txt}="id"></div`;
+        return Object.keys(div.firstChild.dataset)[0]
+        /* Cria atributos para cada div com ID no HTML */
+    }
 }
