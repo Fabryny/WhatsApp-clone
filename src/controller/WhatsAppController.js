a950e04c9ae458f3f50bdb624cabf18fe73e9ceb
Matheus <mfabryny@minsait.com>
2022-11-21 17:05:40 -0300

    Show/hide functions on attach panel

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -127,20 +127,63 @@ class WhatsAppController {
         });
            
         this.el.btnAttachPhoto.on('click', e => {
-            console.log('photo')
+            this.el.inputPhoto.click();
+        });
+
+        this.el.inputPhoto.on('change', e =>{
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file)
+            })
         });
 
         this.el.btnAttachCamera.on('click', e => {
-           console.log('camera')
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 100px)'
+            });
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.el.panelCamera.removeClass('open');
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+        console.log('tirando foto')
         });
 
         this.el.btnAttachDocument.on('click', e => {
-            console.log('documents')
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open')
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 100px)'
+            });
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+            console.log('send document')
         });
 
         this.el.btnAttachContact.on('click', e => {
-            console.log('contact')
+            this.el.modalContacts.show();
         });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+    }
+
+    closeAllMainPanel(){
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
     }
 
     closeMenuAttach(e){
