09425fc60e74f01c2181259d581b04ac91cf5383
Matheus <mfabryny@minsait.com>
2022-12-03 11:06:51 -0300

    Taking picture

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -5,7 +5,7 @@ export default class CameraController {
         navigator.mediaDevices.getUserMedia({/* permissao p/ acessar camera */
             video: true
         }).then((stream) => {
-            
+
             this._stream = stream
             console.log(this._videoEl)
 
@@ -14,5 +14,25 @@ export default class CameraController {
         }).catch(err => {
             console.error(err)
         })
+    }
+
+    stop(){
+        
+        this._stream.getTracks().forEach(track => {
+            track.stop();
+        })
+    }
+
+    takePicture(mimetype = 'image/png'){
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeigth)
+        canvas.setAttribute('width', this._videoEl.videoWidth)
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0 , 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimetype)
     }
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -152,12 +152,35 @@ export default class WhatsAppController {
         this.el.btnClosePanelCamera.on('click', e => {
             this.el.panelCamera.removeClass('open');
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
         });
 
         this.el.btnTakePicture.on('click', e => {
-        console.log('tirando foto')
+          let dataUrl = this._camera.takePicture();
+          
+          this.el.pictureCamera.src = dataUrl;
+          this.el.pictureCamera.show();
+          this.el.videoCamera.hide();
+          this.el.btnReshootPanelCamera.show();
+
+          this.el.containerTakePicture.hide();
+          this.el.containerSendPicture.show();
+
+        });
+        
+        this.el.btnReshootPanelCamera.on('click', e => {
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+  
         });
 
+        this.el.btnSendPicture.on('click', e => {
+            console.log(this.el.pictureCamera.src)
+        });
+            
         this.el.btnAttachDocument.on('click', e => {
             this.closeAllMainPanel();
             this.el.panelDocumentPreview.addClass('open')
@@ -226,8 +249,6 @@ export default class WhatsAppController {
 
         this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji =>{
             emoji.on('click', e=> {
-                console.log(emoji)
-
                 let img = this.el.imgEmojiDefault.cloneNode();
                 img.style.cssText = emoji.style.cssText;
                 img.dataset.unicode = emoji.dataset.unicode;
